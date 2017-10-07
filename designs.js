00d98d0ab66bc62e1d83b8a1292d2eda7191fa92
Coutinho, Reuben <reuben.coutinho@accenture.com>
2017-10-07 15:45:05 +1100

    Chore: updated js file to created grid and add color

--- designs.js
+++ designs.js
@@ -1,10 +1,34 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
+var height, width, color;
 
 function makeGrid() {
-
-// Your code goes here!
-
+    var table = $("#pixel_canvas");
+    table.empty();
+    for (var row = 0; row < height; row++) {
+        var table_row = $("<tr></tr>")
+        for (var column = 0; column < width; column++) {
+            table_row.append("<td></td>")
+        }
+        table.append(table_row);
+    }
 }
+
+$(function () {
+    color = $("#colorPicker").val();
+    console.log(color)
+    $("#colorPicker").change(function () {
+        color = $(this).val();
+        console.log(color)
+    })
+
+    $("#sizePicker").submit(function (event) {
+        event.preventDefault()
+        height = $("#input_height").val();
+        width = $("#input_width").val();
+        makeGrid();
+    })
+
+    $("#pixel_canvas").on("click", "td", function (evt) {
+        $(evt.target).css("background-color", color);
+    })
+
+})
