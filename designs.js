c9414b04a333a1f66527cf44da3cef0c02fac1e9
Coutinho, Reuben <reuben.coutinho@accenture.com>
2017-10-07 22:33:41 +1100

    Refactor: Added comments

--- designs.js
+++ designs.js
@@ -1,34 +1,35 @@
-var height, width, color;
+var color;
 
-function makeGrid() {
+/**
+* @description creates a table grid using the parameters provided
+* @param {number} height height of the grid
+* @param {number} width width of the grid
+*/
+function makeGrid(height, width) {
     var table = $("#pixel_canvas");
     table.empty();
     for (var row = 0; row < height; row++) {
-        var table_row = $("<tr></tr>")
+        var table_row = $("<tr></tr>");
         for (var column = 0; column < width; column++) {
-            table_row.append("<td></td>")
+            table_row.append("<td></td>");
         }
         table.append(table_row);
     }
 }
 
 $(function () {
+    //Initially save the color when document is ready
     color = $("#colorPicker").val();
-    console.log(color)
     $("#colorPicker").change(function () {
         color = $(this).val();
-        console.log(color)
     })
 
     $("#sizePicker").submit(function (event) {
-        event.preventDefault()
-        height = $("#input_height").val();
-        width = $("#input_width").val();
-        makeGrid();
+        event.preventDefault();
+        makeGrid($("#input_height").val(),$("#input_width").val());
     })
 
     $("#pixel_canvas").on("click", "td", function (evt) {
         $(evt.target).css("background-color", color);
     })
-
-})
+});
